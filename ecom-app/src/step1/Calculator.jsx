c951b08d779e7c1a54c6ccf69ac338eaca918e8e
omd-24 <omdeshmukh240@gmail.com>
2026-02-22 11:57:13 +0530

    changes approved

---
+++ ecom-app/src/step1/Calculator.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+const Calculator = () => {
+  return (
+    <>
+    <h1>Calculator App</h1>
+    <Addition/>
+    <Substraction/>
+    <Multiplication/>
+    <Division/>
+    </>
+  )
+}
+
+export default Calculator
