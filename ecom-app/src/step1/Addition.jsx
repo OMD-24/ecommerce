83a136c9d1deabec260770081854fb48a47e8c65
sarthakchoramale09-ship-it <sarthakchoramale09@gmail.com>
2026-02-22 12:18:54 +0530

    changes

--- ecom-app/src/step1/Addition.jsx
+++ ecom-app/src/step1/Addition.jsx
@@ -1,9 +1,13 @@
 
 
 let Addition = ( ) =>{
+    let addtion_click = () =>{
+        alert("Addition click");
+    }
+
     return (
         <div> 
-        <p>This is Addition</p>
+        <button classname="border-1 bg-green-900 hover:bg-blue-900" onClick={()=>addition_click()}>This is Addition</button>
         </div>
 
     )
