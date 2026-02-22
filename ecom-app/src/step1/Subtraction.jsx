17839c023b0785f6b8a5cc6e73af91297fb6b25f
sarthakchoramale09-ship-it <sarthakchoramale09@gmail.com>
2026-02-22 14:38:26 +0530

    change

--- ecom-app/src/step1/Subtraction.jsx
+++ ecom-app/src/step1/Subtraction.jsx
@@ -1,7 +1,13 @@
-import React from "react";
+let Subtraction = ({x,y}) =>{
+    let subtraction_click = () =>{
+        alert("Subtraction ="+ x-y);
+    }
 
-const Subtraction = ({ x, y }) => {
-  return <h3>Subtraction: {x - y}</h3>;
-};
+    return (
+        <div> 
+        <button classname="border-1 bg-green-900 hover:bg-blue-900" onClick={()=>subtraction_click()}> Subtraction</button>
+        </div>
 
-export default Subtraction;
+    )
+} 
+export default Multiplication;
